3ae7bf9ea51ccc84914139732293194b95081924
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-10-12 21:52:03 -0400

    finished contract, waiting for gas

--- contracts/SelfieNFT.sol
+++ contracts/SelfieNFT.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: Unlicensed
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract SelfieNFT {
-  constructor(){
-    console.log("Testing!");
-  }
+contract SelfieNFT is ERC721URIStorage {
+
+
+   using Counters for Counters.Counter;
+   Counters.Counter private _tokenIds;
+
+   constructor() ERC721 ("SelfieNFT", "SELFIE"){
+     console.log("This is my SelfieNFT");
+   }
+
+   function mintSelfieNFT() public {
+     uint256 newItemId = _tokenIds.current();
+
+     _safeMint(msg.sender, newItemId);
+
+     _setTokenURI(newItemId, "ipfs://QmbXfhQXK22ctPgZPNQVykJhexD7Pty9eRcdeMiirtZrWr");
+
+     _tokenIds.increment();
+   }
 }
